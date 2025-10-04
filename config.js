9ca8cb10545b476e3cd23aae9dffb5381c715352
gagegfg <89542627+gagegfg@users.noreply.github.com>
2025-10-04 01:12:47 -0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,7 @@
+// ¡IMPORTANTE!
+// 1. Añade este archivo a tu .gitignore para no subir tus claves a GitHub.
+// 2. Para probar localmente, reemplaza los valores de abajo con tus claves reales de Supabase.
+// 3. En Vercel, este archivo se debe generar durante el proceso de build usando las variables de entorno.
+
+const SUPABASE_URL = "PON_AQUI_TU_URL_DE_SUPABASE";
+const SUPABASE_KEY = "PON_AQUI_TU_KEY_ANON_DE_SUPABASE";
